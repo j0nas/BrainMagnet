e9ade56d9e00b8c18b23f00f66c4ce16034d8bf0
eivindveg <eivindveg@hotmail.com>
2015-04-23 14:32:06 +0200

    update generate_report feature to include variables
    this commit entirely regenerates the steps (updating commit 7843f10832343a4d35eb6fdf66deb2636cbec2d4)

--- features/step_definitions/generate_report.steps.rb
+++ features/step_definitions/generate_report.steps.rb
@@ -1,3 +1,71 @@
 Given(/^the patient's name is "([^"]*)"$/) do |arg|
   pending
 end
+
+And(/^"([^"]*)"'s assigned therapist's name is "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+And(/^an unassigned therapist's name is "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^another patient is named "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^"([^"]*)" has entered data the past week$/) do |arg|
+  pending
+end
+
+Given(/^"([^"]*)" is logged in to the dashboard$/) do |arg|
+  pending
+end
+
+When(/^"([^"]*)" clicks "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+Then(/^"([^"]*)"'s report should be shown$/) do |arg|
+  pending
+end
+
+Given(/^the report is attempted viewed$/) do
+  pending
+end
+
+When(/^the report does not exist$/) do
+  pending
+end
+
+Then(/^the report should be generated$/) do
+  pending
+end
+
+And(/^the report should be shown$/) do
+  pending
+end
+
+When(/^"([^"]*)" views a list of reports$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" should see all reports from their patients$/) do |arg|
+  pending
+end
+
+But(/^"([^"]*)" should not see "([^"]*)"'s reports$/) do |arg1, arg2|
+  pending
+end
+
+When(/^"([^"]*)" views his reports$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" should see his reports$/) do |arg|
+  pending
+end
+
+But(/^"([^"]*)"'s reports should not be visible$/) do |arg|
+  pending
+end
